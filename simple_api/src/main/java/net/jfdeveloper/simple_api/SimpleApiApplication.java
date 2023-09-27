328529e820260a9962b34e587140cfd87861981b
Jim Doyle <indellibleink@gmail.com>
2023-09-27 18:12:39 -0400

    Created Project

---
+++ simple_api/src/main/java/net/jfdeveloper/simple_api/SimpleApiApplication.java
@@ -0,0 +1,13 @@
+package net.jfdeveloper.simple_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+
+}
---
+++ simple_api/src/test/java/net/jfdeveloper/simple_api/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jfdeveloper.simple_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
