a13bce74ff9436a5b5dee03746a88011715e1fd5
Jim Doyle <indellibleink@gmail.com>
2023-09-28 01:01:54 -0400

    Added test controller

---
+++ simple_api/src/test/java/net/jfdeveloper/simple_api/controllers/TestController.java
@@ -0,0 +1,44 @@
+package net.jfdeveloper.simple_api.controllers;
+
+// import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+// @CrossOrigin
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/public")
+    public String allAccess() {
+    
+        return "public content";
+    }
+
+    @GetMapping("/user")
+//    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User content";
+    }
+
+    @GetMapping("/mod")
+//    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String modAccess() {
+        return "Mod content";
+    }
+
+    @GetMapping("/admin")
+//    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "admin content";
+    }
+    
+    @GetMapping("/data")
+    public static void getAPIData() {
+    	System.out.println("get some data");
+     //  return "data content";
+    }
+    
+    
+}
+
